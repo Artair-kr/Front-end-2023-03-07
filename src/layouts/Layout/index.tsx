de603b97c918886a33c8b357a882310764d50824
Artair-kr <hjs6855@naver.com>
2025-03-11 11:51:28 +0900

    2025-03-11 11:50 layout / main 페이지 top-bar 작성

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -4,13 +4,30 @@ import { Outlet } from 'react-router'
 // 스타일 적용
 import './style.css'
 
-// component : 로그인 회원가입 화면 컴포넌트 //
+// component: 공통 레이아웃 컴포넌트 //
 export default function Layout() {
 
-  // render : 로그인 회원가입 화면 컴포넌트 렌더링 //
+  // render: 공통 레이아웃 컴포넌트 렌더링 //
   return (
-    <div>
+    <div id='layout-wrapper'>
+      <div id='top-bar'>
+        <div className='navigation'>
+          <div className='title'>Memories</div>
+          <div className='navigation-list'>
+            <div className='navigation-item active'>기억력 검사</div>
+            <div className='navigation-item'>집중력 검사</div>
+          </div>
+        </div>
+        <div className='my-content'>
+          <div className='my-content-list'>
+            <div className='my-content-item'>일기</div>
+            <div className='my-content-item'>로그아웃</div>
+          </div>
+        </div>
+      </div>
+      <div id='main'>
         <Outlet />
+      </div>
     </div>
   )
 }

7e19d2773f4476f158ba756943ca84535ef5c0a2
Artair-kr <hjs6855@naver.com>
2025-03-11 12:25:24 +0900

    2025-03-11 12:30 layout / 클릭 이벤트

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Outlet } from 'react-router'
 
 // 스타일 적용
@@ -6,6 +6,34 @@ import './style.css'
 
 // component: 공통 레이아웃 컴포넌트 //
 export default function Layout() {
+
+  // state: My Content List 요소 참조 //
+  // (null) : 초기에는 null값이 들어간다.
+  const myContentListRef = useRef<HTMLDivElement | null>(null);
+
+  // state: My Content 드롭다운 상태 //
+  const [showMyContent, setShowMyContent] = useState<boolean>(false);
+
+  // event handler: My Content 클릭 이벤트 처리 //
+  const onMyContentClickHandler = () => {
+    setShowMyContent(!showMyContent);
+  };
+
+  // effect: My Content 드롭다운 상태가 변경될시 실행할 함수 //
+  useEffect(() => {
+    const onOutsideClickHandler = (event: any) => {
+      if(myContentListRef.current && 
+        !myContentListRef.current.contains(event.target as Node)
+      ){
+        setShowMyContent(false);
+      }
+    };
+
+    if(!showMyContent) return;
+
+    document.addEventListener('mousedown', onOutsideClickHandler)
+
+  }, [showMyContent]);
 
   // render: 공통 레이아웃 컴포넌트 렌더링 //
   return (
@@ -18,11 +46,13 @@ export default function Layout() {
             <div className='navigation-item'>집중력 검사</div>
           </div>
         </div>
-        <div className='my-content'>
-          <div className='my-content-list'>
+        <div className='my-content' onClick={onMyContentClickHandler}>
+          {showMyContent && 
+          <div ref={myContentListRef} className='my-content-list'>
             <div className='my-content-item'>일기</div>
             <div className='my-content-item'>로그아웃</div>
           </div>
+          }
         </div>
       </div>
       <div id='main'>

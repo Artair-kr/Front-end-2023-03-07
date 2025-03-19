1d149229cb334dbfe53e00e2f5c412bf0f2d0814
Artair-kr <hjs6855@naver.com>
2025-03-19 14:34:59 +0900

    2025-03-19 14:30

--- src/types/interfaces/diaty.interface.ts
+++ src/types/interfaces/diaty.interface.ts
@@ -1,6 +1,7 @@
 import { Feeling, Weather } from "../aliases";
 
 export default interface Diary {
+    diaryNumber: string | number;
     writeDate: string;
     title: string;
     weather: Weather;

9015fcccbdb21d48aa57740a12842dcb7f73d7aa
Artair-kr <hjs6855@naver.com>
2025-03-20 14:52:18 +0900

    2025-03-20 14:50

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { ACCESS_TOKEN, AUTH_ABSOLUTE_PATH, AUTH_PATH, CONCENTRATION_TEST_COMPLET
 import DiaryMain from './views/Diary';
 import DiaryWrite from './views/Diary/Write';
 import DiaryDetail from './views/Diary/Detail';
+import DiaryUpdate from './views/Diary/Update';
 
 
 // Router 구성
@@ -51,7 +52,7 @@ function App() {
           <Route path={DIARY_WRITE_PATH} element={<DiaryWrite />} />
           <Route path={DIARY_VIEW_PATH}>
             <Route index element={<DiaryDetail/>} />
-            <Route path={DIARY_UPDATE_PATH} element={<>일기 수정 페이지</>} />
+            <Route path={DIARY_UPDATE_PATH} element={<DiaryUpdate/>} />
           </Route>
         </Route>
 

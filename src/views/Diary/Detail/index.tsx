2bad3a713f8b8463492c6eedd78d6df6dc9a81ed
Artair-kr <hjs6855@naver.com>
2025-03-20 11:11:23 +0900

    2025-03-20 10:50

--- src/views/Diary/Detail/index.tsx
+++ src/views/Diary/Detail/index.tsx
@@ -6,8 +6,8 @@ export default function DiaryDetail() {
 
     //render: 일기 상세 화면 컴포넌트 렌더링 //
     return (
-        <div id='diary-write-wrapper'>
-          <div  className='detail-container'></div>
+        <div id='diary-detail-wrapper'>
+          <div className='detail-container'></div>
         </div>
     )
 }

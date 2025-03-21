ce3659bde1ac2f9ff1267e4bb1202f9abdcca972
Artair-kr <hjs6855@naver.com>
2025-03-21 16:01:40 +0900

    2025-03-21 15:50

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import DiaryWrite from './views/Diary/Write';
 import DiaryDetail from './views/Diary/Detail';
 import DiaryUpdate from './views/Diary/Update';
 import MemoryTest from './views/MemoryTest';
+import MemoryTestComplete from './views/MemoryTest/Complete';
 
 
 // Router 구성
@@ -40,7 +41,7 @@ function App() {
 
         <Route path={MEMORY_TEST_PATH}>
           <Route index element={<MemoryTest />} />
-          <Route path={MEMORY_TEST_COMPLETE_PATH} element={<>기억력 검사 완료 페이지</>} />
+          <Route path={MEMORY_TEST_COMPLETE_PATH} element={<MemoryTestComplete />} />
         </Route>
 
         <Route path={CONCENTRATION_TEST_PATH}>

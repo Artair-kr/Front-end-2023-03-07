0d747ec2589ccb23ce44bf117e8d85542236a196
Artair-kr <hjs6855@naver.com>
2025-03-25 12:23:53 +0900

    2025-03-25 12:30

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DefaultProfile from 'src/assets/images/default-profile.png'
 import './style.css';
 
 // interface: 메인 화면 컴포넌트 //
@@ -6,7 +7,11 @@ export default function Main(){
   // reder: 메인 화면 컴포넌트 렌더링 //
   return ( 
     <div id='main-wrapper'>
-      <div className="user-info-container"></div>
+      <div className="user-info-container">
+        <div className="profile-image" style={{ backgroundImage: `url(${DefaultProfile})` }}></div>
+        <div className="user-info-box"></div>
+        <div className="button second middle">수정</div>
+      </div>
       <div className="recently-container"></div>
     </div>
   )
